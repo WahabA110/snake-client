e1f75a198e692c518a22da82fac57fded7500584
Wahab Alamin <wahabalamin@gmail.com>
2021-08-25 19:54:39 -0400

    refactored code so the file only contains the requires and the necessary function calls to run the program

--- play.js
+++ play.js
@@ -1,27 +1,11 @@
 const net = require('net');
 const {connect} = require('./client');
-// const {setupInput} = require('./input')
+const {setupInput} = require('./input')
 
 // const host = '135.23.223.133'
 // const port = 50542
 
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
-
-const setupInput = function () {
-
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-
-};
-
-setupInput()
 console.log("Connecting ...");
 connect();
+
+setupInput()
