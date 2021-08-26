e14d8cd92a84f5ee8f9e948c1d40a41880045c23
Wahab Alamin <wahabalamin@gmail.com>
2021-08-25 21:54:08 -0400

    minor touch up to remove blocked out code

--- play.js
+++ play.js
@@ -2,8 +2,6 @@ const net = require('net');
 const {connect} = require('./client');
 const {setupInput} = require('./input')
 
-// const host = '135.23.223.133'
-// const port = 50542
 
 console.log("Connecting ...");
 // connect();
