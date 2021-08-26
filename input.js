610ddea38eda66ee8fd4a1db7df90ce13082b44f
Wahab Alamin <wahabalamin@gmail.com>
2021-08-25 21:39:15 -0400

    updated file to send wasd inputs to server to mean move up, left, down, and right. Also passed a parameter into setupInput to assign the value to connection

--- input.js
+++ input.js
@@ -1,13 +1,31 @@
 const net = require('net');
+const { connect } = require('./client');
+
+let connection;
 
 const handleUserInput = function (key) {
   if (key === '\u0003') {
     process.exit();
   }
+  if (key === 'w') {
+    connection.write('Move: up');
+  }
+  if (key === 'a') {
+    connection.write('Move: left');
+  }
+  if (key === 's') {
+    connection.write('Move: down');
+  }
+  if (key === 'd') {
+    connection.write('Move: right');
+  }
+  if (key === 'm') {
+    connection.write('Say: Hello');
+  }
 };
 
-const setupInput = function () {
-
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
